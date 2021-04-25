1e030b4520e3bcf1d13522faf25acd54bdfdbfba
李承斌 <15031617367@163.com>
2021-04-25 16:49:28 +0800

    搜索布局及路由跳转完成

--- src/views/home/Nearby.vue
+++ src/views/home/Nearby.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="nearby">
     <h3 class="nearby_title">附近店铺</h3>
-    <ShopInfo v-for="item in nearbyList" :key="item._id" :item="item" :hideBorder="true"/>
+    <router-link
+    v-for="item in nearbyList"
+    :key="item._id"
+    to="/shop">
+      <ShopInfo :item="item" :hideBorder="true"/>
+    </router-link>
   </div>
 </template>
 <script>
@@ -41,39 +46,8 @@ export default {
     color: $content-fontColor;
     font-weight: normal;
   }
-  &_item {
-    display: flex;
-    padding-top: 0.12rem;
-    &_img {
-      width: 0.56rem;
-      height: 0.56rem;
-      margin-right: 0.16rem;
-    }
-  }
-  &_content {
-    flex: 1;
-    padding-bottom: 0.12rem;
-    border-bottom: 1px solid $content-bgColor;
-    &_title {
-      font-size: 0.16rem;
-      line-height: 0.22rem;
-      color: $content-fontColor;
-    }
-    &_tags {
-      margin-top: 0.08rem;
-      line-height: 0.18rem;
-      font-size: 0.13rem;
-      color: $content-fontColor;
-    }
-    &_tag {
-      margin-right: 0.16rem;
-    }
-    &_highlight {
-      color: #e93b3b;
-      line-height: 0.18rem;
-      font-size: 0.13rem;
-      margin: 0.08rem 0 0 0;
-    }
+  a{
+    text-decoration: none;
   }
 }
 </style>
--- src/views/shop/Shop.vue
+++ src/views/shop/Shop.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="wrapper">
+    <div class="search">
+      <div class="search_back" @click="handleBackClick">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#iconback"></use>
+        </svg>
+      </div>
+      <div class="search_content">
+        <span class='search_content_icon'>
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#iconseachx"></use>
+          </svg>
+        </span>
+        <input class="search_content_input" placeholder="请输入商品名称"/>
+      </div>
+    </div>
     <ShopInfo :item="item" :hideBorder="false"/>
   </div>
 </template>
 <script>
+import { useRouter } from 'vue-router'
 import ShopInfo from '../../components/shopInfo'
 export default {
   name: 'shop',
@@ -11,6 +27,7 @@ export default {
     ShopInfo
   },
   setup () {
+    const router = useRouter()
     const item = {
       _id: '1',
       name: '沃尔沃',
@@ -20,14 +37,60 @@ export default {
       expressPrice: 5,
       slogan: 'VIPxxxxxxxx'
     }
+    const handleBackClick = () => {
+      router.back()
+    }
     return {
-      item
+      item,
+      handleBackClick
     }
   }
 }
 </script>
 <style lang="scss" scoped>
+@import "../../style/virables.scss";
+@import "../../style/mixins.scss";
 .wrapper{
   padding: 0 .18rem;
+}
+.search{
+  margin: .2rem 0 .16rem 0;
+  display: flex;
+  &_back{
+    .icon{
+      fill:#b6b6b6
+    }
+    width: .3rem;
+    font-size: .24rem;
+  }
+  &_content{
+    display: flex;
+    flex:1;
+    background: #f5f5f5;
+    border-radius: .16rem;
+    &_icon{
+      width: .44rem;
+      text-align: center;
+      font-size: .2rem;
+      line-height: .32rem;
+      .icon{
+        fill:#b7b7b7
+      }
+    }
+    &_input{
+      height: .32rem;
+      border: none;
+      outline: none;
+      background: none;
+      display: block;
+      width: 100%;
+      padding-right:.2rem;
+      color: #333;
+      font-size: .14rem;
+      &::placeholder{
+        color: #333;
+      }
+    }
+  }
 }
 </style>
